c1e732832af1aacda783b8cd4d51f7f73e7f3f7a
Reikhadui <reikhadui@gmail.com>
2016-06-06 15:38:28 -0700

    Pushing Initialized environment

---
+++ src/main/java/com/reikhadui/omfb/OhMyFunnyBuckets.java
@@ -0,0 +1,31 @@
+package com.reikhadui.omfb;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="omfb", name="OhMyFunnyBuckets", version="1.7.10-1.0")
+public class OhMyFunnyBuckets
+{
+    @Mod.Instance("OhMyFunnyBuckets")
+    public static OhMyFunnyBuckets instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLPostInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
